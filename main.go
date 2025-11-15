c40885807c1c308c8bf24e146858100fb989c675
bkyryliuk <Bohdan.Kyryliuk_ext@leasingmarkt.de>
2025-11-15 16:58:59 +0200

    Move HTTP server setup to separate web package

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"Golang/GoPlayground"
-	"Golang/HttpHandler"
 	"Golang/greeter"
 	"fmt"
 	"log"
-	"net/http"
 	"time"
 
 	"rsc.io/quote"
@@ -108,13 +106,5 @@ func main() {
 	GoPlayground.Playground()
 
 	// Setting up HTTP server with handlers
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", HttpHandler.HelloHandler)
-	mux.HandleFunc("/count", HttpHandler.CounterHandler)
-
-	log.Println("Listening on :3000")
-
-	if err := http.ListenAndServe(":3000", mux); err != nil {
-		log.Fatal(err)
-	}
+	//web.StartServer()
 }
