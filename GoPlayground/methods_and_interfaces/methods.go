f5943d7f8a7b67c3f00e94cf92011e1d29445869
bkyryliuk <Bohdan.Kyryliuk_ext@leasingmarkt.de>
2025-11-27 20:07:01 +0200

    Add Abs function and refactor Scale method in Vertex demonstration

--- GoPlayground/methods_and_interfaces/methods.go
+++ GoPlayground/methods_and_interfaces/methods.go
@@ -20,6 +20,15 @@ func (v *Vertex) Scale(f float64) {
 	v.Y = v.Y * f
 }
 
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Scale(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func MethodsExample() {
 	v := Vertex{3, 4}
 	fmt.Println(v.Abs())
@@ -27,4 +36,7 @@ func MethodsExample() {
 	v.Scale(10)
 	fmt.Println(v.Abs())
 	fmt.Println(v.X, v.Y)
+
+	Scale(&v, 10)
+	fmt.Println(Abs(v))
 }
